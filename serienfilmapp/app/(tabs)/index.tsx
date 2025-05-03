c78b645edb36661633306ed05827daa41be27488
monster112H <hannes.neuhaus@icloud.com>
2025-05-03 22:08:32 +0200

    asyncstorge funktioniert und zeigt daten passend an mit einem key

--- serienfilmapp/app/(tabs)/index.tsx
+++ serienfilmapp/app/(tabs)/index.tsx
@@ -1,20 +1,18 @@
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons"
-import * as AsyncStorage from "../../utils/AsyncStorage.js"
-import React from "react";
-import { sortRoutes } from "expo-router/build/sortRoutes.js";
+import { Ionicons } from "@expo/vector-icons";
+import * as AsyncStorage from "../../utils/AsyncStorage.js";
+import React, { useEffect, useState } from "react";
+import DATA from "../../data.json";
 
-interface Serie {
+type Serie = {
+  id: number;
   titel: string;
   season: number;
   episode: number;
-}
-
-
+};
 
 const addEpisode = () => {
-
   //alert(JSON.stringify(getData()));
 };
 
@@ -23,89 +21,88 @@ const deleteSerie = () => {
 };
 
 export default function Index() {
-  const counter = 0;
+  const [counter, setCounter] = useState(0);
 
-  const [series, setSeries] = React.useState();
+  const [data, setData] = useState<Serie[] | null>(null);
 
-  const getData = async () => {
-    const values = await AsyncStorage.getItem('@data');
-    setSeries(JSON.parse(values));
-  }
-
-  const addSeason = () => {
-
-    AsyncStorage.setItem('@data', [{
-      "titel": "Serie 1",
-      "season": 2,
-      "episode": 1
-    }, {
-      "titel": "Serie 3",
-      "season": 2,
-      "episode": 1
-    }]
-    );
-
+  const save = async () => {
+    AsyncStorage.setItem("@data", [
+      {
+        titel: "Serie",
+        season: 2,
+        episode: 1,
+      },
+      {
+        titel: "Serie",
+        season: 2,
+        episode: 5,
+      },
+      {
+        titel: "Serie",
+        season: 4,
+        episode: 1,
+      },
+    ]);
+    load();
   };
 
-  //React.useEffect(getData);
+  const load = async () => {
+    setData(await AsyncStorage.getItem("@data"));
+  };
+
+  useEffect(() => {
+    load();
+    console.log(data);
+  }, []);
 
   return (
     <View style={styles.page}>
-      <ScrollView >
+      <ScrollView>
+        {data ? (
+          data.map((item,index) => {
+            return (
+              <FilmElement
+                id={index}
+                titel={item.titel}
+                season={item.season}
+                episode={item.episode}
+              />
+            );
+          })
+        ) : (
+          <Text style={{ color: "white" }}>Loading...</Text>
+        )}
 
-        {series ? <FilmElement titel={series} season={series} episode={series} /> : <Text>Noch keine Serien</Text>}
-
-
-        <TouchableOpacity onPress={addSeason}>
+        <TouchableOpacity onPress={() => save()}>
           <View style={{ alignItems: "center", margin: 25 }}>
             <Ionicons name="add-sharp" style={styles.addIcon} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={getData}>
-          <View style={{ alignItems: "center", margin: 25 }}>
-            <Ionicons name="add" style={styles.addIcon} />
           </View>
         </TouchableOpacity>
       </ScrollView>
     </View>
   );
 }
-/*
-  {data.map((item) => {
-          return (
-            <FilmElement titel={item.titel} season={item.season} episode={item.episode} />
-          )
-        })}
-          */
 
-
-const FilmElement = ({ titel, season, episode }: Serie) => {
-
+const FilmElement = ({ titel, season, episode,id }: Serie) => {
   return (
     <View style={styles.card}>
       <View style={styles.cardContent}>
         <View style={styles.cardHeader}>
-          <Text style={styles.seriesTitle}>
-            {titel}
-          </Text>
+          <Text style={styles.seriesTitle}>{titel} {id}</Text>
           <TouchableOpacity onPress={deleteSerie}>
             <Ionicons name="checkmark-done" style={styles.icon} />
           </TouchableOpacity>
         </View>
 
         <View style={styles.cardBody}>
-          <View style={{ flexDirection: 'row', gap: 15, alignItems: 'center', }}>
-            <Text style={styles.seriesText}>
-              Staffel:  {season}
-            </Text>
+          <View style={{ flexDirection: "row", gap: 15, alignItems: "center" }}>
+            <Text style={styles.seriesText}>Staffel: {season}</Text>
             <TouchableOpacity>
               <Ionicons name="add-circle-outline" style={styles.icon} />
             </TouchableOpacity>
           </View>
-          <View style={{ flexDirection: 'row', gap: 15, alignItems: 'center', }}>
-            <Text style={styles.seriesText}>
-              Folge:  {episode}
-            </Text>
+          <View style={{ flexDirection: "row", gap: 15, alignItems: "center" }}>
+            <Text style={styles.seriesText}>Folge: {episode}</Text>
             <TouchableOpacity onPress={addEpisode}>
               <Ionicons name="add-circle-outline" style={styles.icon} />
             </TouchableOpacity>
@@ -114,26 +111,22 @@ const FilmElement = ({ titel, season, episode }: Serie) => {
       </View>
     </View>
   );
-}
-
-
-
+};
 
 const styles = StyleSheet.create({
-
   page: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: '#000',
+    backgroundColor: "#000",
   },
 
   card: {
-    backgroundColor: '#1A1A1A',
+    backgroundColor: "#1A1A1A",
     borderRadius: 16,
     borderColor: "#8b8c8b",
-    borderWidth: .2,
+    borderWidth: 0.2,
     marginBottom: 16,
-    shadowColor: '#8b8c8b',
+    shadowColor: "#8b8c8b",
     shadowOffset: {
       width: 0,
       height: 0,
@@ -143,7 +136,7 @@ const styles = StyleSheet.create({
 
     marginBlock: 10,
     marginInline: 20,
-    height: 80
+    height: 80,
   },
 
   cardContent: {
@@ -151,19 +144,19 @@ const styles = StyleSheet.create({
   },
 
   cardHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 12,
   },
 
   seriesTitle: {
     fontSize: 20,
     //fontFamily: 'Inter_600SemiBold',
-    color: 'white',
+    color: "white",
     fontWeight: "bold",
 
-    textShadowColor: '#8b8c8b',
+    textShadowColor: "#8b8c8b",
     textShadowOffset: {
       width: 2,
       height: 2,
@@ -174,7 +167,7 @@ const styles = StyleSheet.create({
   icon: {
     color: "white",
     fontSize: 20,
-    textShadowColor: '#94e0a0',
+    textShadowColor: "#94e0a0",
     textShadowOffset: {
       width: 1,
       height: 1,
@@ -183,22 +176,21 @@ const styles = StyleSheet.create({
   },
 
   cardBody: {
-    flexDirection: 'row',
+    flexDirection: "row",
     //justifyContent: 'space-evenly',
     gap: 40,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 12,
   },
 
   seriesText: {
     color: "white",
-    fontWeight: "500"
+    fontWeight: "500",
   },
 
   addIcon: {
     color: "white",
     //fontWeight: "500",
     fontSize: 35,
-
-  }
-})
+  },
+});
--- serienfilmapp/app/(tabs)/other.tsx
+++ serienfilmapp/app/(tabs)/other.tsx
@@ -1,22 +1,77 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { StyleSheet } from "react-native";
-import { Ionicons} from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import * as AsyncStorage from "../../utils/AsyncStorage.js";
+
 
 export default function Other() {
+
+  const save = async () =>{
+    AsyncStorage.setItem("myName",name);
+  }
+
+  const load = async () => {
+    setName(await AsyncStorage.getItem("myName"));
+  }
+
+  
+
+  const [name,setName] = useState(""); 
+
+
+  useEffect(() => {
+    load()
+  },[]);
+
   return (
     <View style={styles.page}>
-      <Text>Coming Soon!!!</Text>
+      <Text>Dein Name:</Text>
+
+      <TextInput style={styles.text} onChangeText={(text) => setName(text)} />
+
+      <TouchableOpacity style={styles.button} onPress={() => save()}>
+        <Text> SAVE</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text> REMOVE</Text>
+      </TouchableOpacity>
+
+      <Text style={{color: "white"}}>{name}</Text>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
-
   page: {
     flex: 1,
-    //justifyContent: "center",
-    //backgroundColor: '#000'
+    alignItems: "center",
+    backgroundColor: "#000",
   },
-})
+
+  text: {
+    borderWidth: 1,
+    borderColor: "white",
+    alignSelf: "stretch",
+    height: 60,
+    borderRadius: 6,
+    fontSize: 20,
+    color: "white",
+    paddingHorizontal: 20,
+    margin: 32,
+  },
+
+  button: {
+    backgroundColor: "#AAABBB",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "stretch",
+    height: 60,
+    borderRadius: 6,
+    fontSize: 20,
+    color: "white",
+    paddingHorizontal: 20,
+    margin: 32,
+  },
+});
